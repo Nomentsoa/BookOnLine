e48b303ce50b7196984f310587a5575932c42319
sedera_nomentsoa <starksedera@gmail.com>
2020-12-17 14:30:34 +0300

    services_et_controller_member

---
+++ src/test/groovy/mg/sederanomentsoa/OcbControllerSpec.groovy
@@ -0,0 +1,18 @@
+package mg.sederanomentsoa
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class OcbControllerSpec extends Specification implements ControllerUnitTest<OcbController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
